f937f2e2a35e9bfdde8339f799987c62f804dd62
altjsis0404 <altjsis@naver.com>
2024-02-20 17:33:15 +0900

    20240220_sub_반응형_common

--- script/main/mainHeader.js
+++ script/main/mainHeader.js
@@ -38,4 +38,17 @@ header_btnCall.addEventListener('click', function (e) {
 });
 
 
-
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('product_mo').addEventListener('change', function() {
+        window.location.href = this.value;
+    });
+    document.getElementById('service_mo').addEventListener('change', function() {
+        window.location.href = this.value;
+    });
+    document.getElementById('PRcenter_mo').addEventListener('change', function() {
+        window.location.href = this.value;
+    });
+    document.getElementById('company_mo').addEventListener('change', function() {
+        window.location.href = this.value;
+    });
+});
