6aa896f9dd05c47e8781ae5663378cedbe1e5396
altjsis0404 <altjsis@naver.com>
2024-02-21 19:09:04 +0900

    20240221_sub_반응형

--- script/main.js
+++ script/main.js
@@ -1,2 +1,16 @@
 AOS.init();
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('product_mo').addEventListener('change', function() {
+        window.location.href = this.value;
+    });
+    document.getElementById('service_mo').addEventListener('change', function() {
+        window.location.href = this.value;
+    });
+    document.getElementById('PRcenter_mo').addEventListener('change', function() {
+        window.location.href = this.value;
+    });
+    document.getElementById('company_mo').addEventListener('change', function() {
+        window.location.href = this.value;
+    });
+});
